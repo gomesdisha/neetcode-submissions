4cd68622bf8ccb6278bbaeb479477ed5b950d1d3
Disha Gomes <97966673+gomesdisha@users.noreply.github.com>
2026-09-07 11:59:47 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i=0
+        while i<len(nums)-1 and i+1<len(nums):
+            if nums[i]==nums[i+1] :
+                i+=2
+            else:
+                return nums[i]
+        return nums[len(nums)-1]
+
+        
